665c8f5bed7973786da594c2f02aa87928b02fed
Nuno <nm.cdc@hotmail.com>
2025-01-08 20:06:16 +0000

    added events in the solidity contract based on the documentation and contract examples both to log actions to be retrieved in the be/fe and the function to create the game

--- hardhat/RPS.sol
+++ hardhat/RPS.sol
@@ -21,4 +21,32 @@ contract RockPaperScissors {
     uint256 pot;
     GameStatus status;
   }
+
+  // Creating an address for each unique game and tracking number of games created for book keeping
+  mapping(uint256 => Game) public games;
+  uint256 public gameCounter;
+
+  // Logs for key actions like game creation, second player joining, revealing the moves
+  event GameCreated(uint256 indexed gameId, address indexed player1, uint256 betAmount);
+  event PlayerJoined(uint256 indexed gameId, address indexed player2);
+  event MovesCommitted(uint256 indexed gameId, address indexed player);
+  event MovesRevealed(uint256 indexed gameId, address indexed player, Move move);
+  event GameCompleted(uint256 indexed gameId, address indexed winner, uint256 pot);
+
+  function createGame(bytes32 hashedMove) externable payable returns(unit256) {
+    require(msg.value > 0, "Bet amount must be greater than zero");
+    require(hashedMove != bytes32(0), "Move is required");
+    
+    gameCounter++;
+
+    Game storage game = games[gameCounter];
+
+    game.player1 = Player(payable(msg.sender), hashedMove, Move.None);
+    game.pot = msg.value;
+    game.status = GameStatus.WaitingForPlayers;
+
+    emit GameCreated(gameCounter, msg.sender, msg.value);
+
+    return gameCounter
+  }
 }
